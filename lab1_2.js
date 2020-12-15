bda28700011fdde25251457f15e9f0dca2d5bcc0
AJSTIP <70164041+AJSTIP@users.noreply.github.com>
2020-12-15 16:20:45 -0500

    Add files via upload

---
+++ lab1_2.js
@@ -0,0 +1,23 @@
+var numbersOfApples = 10;
+document.write("The numbers of apples I have is " + numbersOfApples + "<br>");
+numbersOfApples++;
+document.write("The numbers of apples I have is " + numbersOfApples + "<br>");
+numbersOfApples++;
+document.write("The numbers of apples I have is " + numbersOfApples + "<br>");
+numbersOfApples++;
+document.write("The numbers of apples I have is " + numbersOfApples + "<br>");
+numbersOfApples++;
+document.write("The numbers of apples I have is " + numbersOfApples + "<br>");
+numbersOfApples++;
+document.write("The numbers of apples I have is " + numbersOfApples + "<br>");
+numbersOfApples++;
+
+document.write("<br><br>")
+
+var numberOfNinja = 100;
+document.write("There are " + --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are " + --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are " + --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are " + --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are " + --numberOfNinja + " ninjas in the room. <br><br>")
+document.write("Created by Aj")
